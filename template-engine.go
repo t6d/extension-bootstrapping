888cd262af0f633054ed6cbf84da1e17f1f20f39
Konstantin Tennhard <konstantin@tennhard.net>
2022-03-29 21:34:54 -0400

    Merge by concatenation

--- template-engine.go
+++ template-engine.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"io/fs"
+	"strings"
 	"text/template"
 )
 
 func createTemplateEngine(shared fs.FS) *template.Template {
 	t := template.Template{}
-	t.Funcs(helpers)
+	t.Funcs(helpers(shared))
 
 	fs.WalkDir(shared, ".", func(path string, d fs.DirEntry, err error) error {
 		if !d.IsDir() {
@@ -21,4 +23,17 @@ func createTemplateEngine(shared fs.FS) *template.Template {
 	return &t
 }
 
-var helpers template.FuncMap = template.FuncMap{}
+func helpers(shared fs.FS) template.FuncMap {
+	return template.FuncMap{
+		"merge": func(paths ...string) string {
+			buffer := bytes.Buffer{}
+
+			for _, path := range paths {
+				data, _ := fs.ReadFile(shared, strings.TrimPrefix(path, "shared/"))
+				buffer.Write(data)
+			}
+
+			return string(buffer.Bytes())
+		},
+	}
+}
